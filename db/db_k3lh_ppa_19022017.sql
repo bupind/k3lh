7fadc3ca73be559d67d2d3a6a7639f275dc4695c
Joko Hermanto <joko.hermanto19@gmail.com>
2017-02-19 21:58:31 +0700

    PPA: setup permit - db

---
+++ db/db_k3lh_ppa_19022017.sql
@@ -0,0 +1,91 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.15 
+MySQL - 5.5.25a : Database - k3lh
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `ppa` */
+
+DROP TABLE IF EXISTS `ppa`;
+
+CREATE TABLE `ppa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sector_id` int(11) NOT NULL,
+  `power_plant_id` int(11) NOT NULL,
+  `ppa_year` int(4) NOT NULL,
+  `created_by` smallint(5) unsigned NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_by` smallint(5) unsigned NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_ppa_sector` (`sector_id`),
+  KEY `FK_ppa_power_plant` (`power_plant_id`),
+  CONSTRAINT `FK_ppa_power_plant` FOREIGN KEY (`power_plant_id`) REFERENCES `power_plant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_ppa_sector` FOREIGN KEY (`sector_id`) REFERENCES `sector` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ppa` */
+
+insert  into `ppa`(`id`,`sector_id`,`power_plant_id`,`ppa_year`,`created_by`,`created_at`,`updated_by`,`updated_at`) values (1,4,2,2017,8,1487233107,8,1487233107),(2,7,5,2017,8,1487233182,8,1487233182),(3,2,4,2017,8,1487479047,8,1487479047);
+
+/*Table structure for table `ppa_month` */
+
+DROP TABLE IF EXISTS `ppa_month`;
+
+CREATE TABLE `ppa_month` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ppa_setup_permit_id` int(11) NOT NULL,
+  `ppam_month` int(2) NOT NULL,
+  `ppam_year` int(4) NOT NULL,
+  `ppam_cert_number` varchar(4) DEFAULT NULL,
+  `created_by` smallint(5) unsigned NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_by` smallint(5) unsigned NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_ppa_month_permit` (`ppa_setup_permit_id`),
+  CONSTRAINT `FK_ppa_month_permit` FOREIGN KEY (`ppa_setup_permit_id`) REFERENCES `ppa_setup_permit` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ppa_month` */
+
+insert  into `ppa_month`(`id`,`ppa_setup_permit_id`,`ppam_month`,`ppam_year`,`ppam_cert_number`,`created_by`,`created_at`,`updated_by`,`updated_at`) values (1,1,7,2016,'1205',8,1487513541,8,1487515069),(2,1,8,2016,'1348',8,1487513541,8,1487515069),(3,1,9,2016,'1492',8,1487513541,8,1487515069),(4,1,10,2016,'1797',8,1487513541,8,1487515069),(5,1,11,2016,'1967',8,1487513541,8,1487515069),(6,1,12,2016,'1201',8,1487513541,8,1487515069),(7,1,1,2017,'2429',8,1487513541,8,1487515069),(8,1,2,2017,'2572',8,1487513541,8,1487515069),(9,1,3,2017,'2722',8,1487513541,8,1487515069),(10,1,4,2017,'2940',8,1487513541,8,1487515069),(11,1,5,2017,'',8,1487513541,8,1487515069),(12,1,6,2017,'',8,1487513541,8,1487515069);
+
+/*Table structure for table `ppa_setup_permit` */
+
+DROP TABLE IF EXISTS `ppa_setup_permit`;
+
+CREATE TABLE `ppa_setup_permit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ppa_id` int(11) NOT NULL,
+  `ppasp_wastewater_source` varchar(150) DEFAULT NULL,
+  `ppasp_setup_point_name` varchar(150) DEFAULT NULL,
+  `ppasp_coord_ls` varchar(50) DEFAULT NULL,
+  `ppasp_coord_bt` varchar(50) DEFAULT NULL,
+  `ppasp_wastewater_tech_code` varchar(10) DEFAULT NULL,
+  `ppasp_permit_number` varchar(50) DEFAULT NULL,
+  `ppasp_permit_publisher` varchar(150) DEFAULT NULL,
+  `ppasp_permit_publish_date` date DEFAULT NULL,
+  `ppasp_permit_end_date` date DEFAULT NULL,
+  `created_by` smallint(5) unsigned NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_by` smallint(5) unsigned NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_ppa_setup_permit_ppa` (`ppa_id`),
+  CONSTRAINT `FK_ppa_setup_permit_ppa` FOREIGN KEY (`ppa_id`) REFERENCES `ppa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ppa_setup_permit` */
+
+insert  into `ppa_setup_permit`(`id`,`ppa_id`,`ppasp_wastewater_source`,`ppasp_setup_point_name`,`ppasp_coord_ls`,`ppasp_coord_bt`,`ppasp_wastewater_tech_code`,`ppasp_permit_number`,`ppasp_permit_publisher`,`ppasp_permit_publish_date`,`ppasp_permit_end_date`,`created_by`,`created_at`,`updated_by`,`updated_at`) values (1,1,'PROSES UTAMA (WWTP)','OUTLET WWTP','05&deg; 31\' 20,6\"','105&deg; 21\' 12,7\"','ANAER','109 TAHUN 2012','KEMENTERIAN LINGKUNGAN HIDUP','2012-05-29','2017-05-29',8,1487513541,8,1487515069);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
